5b622c6b83dd111da7cd6150635274efa00d06f1
Stepan Poperechnyi <poperechnyi1@gmail.com>
2018-04-01 19:02:34 +0300

    improve generator

--- learnIterators.js
+++ learnIterators.js
@@ -1,17 +1,32 @@
-let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
+// let xmen = ['Cyclops', 'Wolverine', 'Rogue'];
+//
+// for (let xman of xmen) {
+//     console.log(xman);
+// }
+//
+// let iterator = xmen[Symbol.iterator]();
+// let next = iterator.next();
+//
+// while (!next.done) {
+//     console.log(next.value);
+//     next = iterator.next();
+// }
 
-for (let xman of xmen) {
-    console.log(xman);
-}
+let randomGenerator = {
+    [Symbol.iterator]() {
+        let count = 0;
+        return {
+            next() {
+                let value = Math.ceil(Math.random() * 100);
+                let done = count > 9;
+                count += 1;
+                return { value, done};
+            }
+        };
+    }
+};
 
-let iterator = xmen[Symbol.iterator]();
-let next = iterator.next();
-
-while(!next.done){
-    console.log(next.value);
-    next = iterator.next();
-}
-
-
+let random = randomGenerator[Symbol.iterator]();
+console.log(random.next().value);
 
 
