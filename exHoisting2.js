f234d1c9cca92ee0b4c3c78e3d8d84877d48c6e5
Stepan Poperechnyi <poperechnyi1@gmail.com>
2023-05-02 12:55:18 +0300

    hoisting ex 2

---
+++ exHoisting2.js
@@ -0,0 +1,11 @@
+const favouriteFood = "grapes";
+
+const foodThoughts = function () {
+  console.log("Original favourite food: " + favouriteFood);
+
+  const favouriteFood = "sushi";
+
+  console.log("New favourite food: " + favouriteFood);
+};
+
+foodThoughts();
