9f201362bc671d15b91fb47c684d2caccaa0e979
Stepan Poperechnyi <poperechnyi1@gmail.com>
2023-07-02 19:40:41 +0300

    add other recursion method

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,5 @@
-let array = [];
 function fibonacciViaLoop(n) {
+  let array = [];
   for (let i = 0; i < n; i++) {
     const sum = 0;
     if (array.length === 0) {
@@ -17,3 +17,37 @@ function fibonacciViaLoop(n) {
 }
 
 console.log(fibonacciViaLoop(10));
+
+let array2 = [];
+function fibonacciViaRecursion(n) {
+  if (array2.length === 0 && n > 0) {
+    array2.push(array2.length);
+    return fibonacciViaRecursion(n - 1);
+  }
+
+  if (array2.length > 0 && array2.length === 1 && n > 0) {
+    array2.push(array2.length);
+    return fibonacciViaRecursion(n - 1);
+  }
+
+  if (array2.length > 0 && array2.length > 1 && n > 0) {
+    array2.push(array2[array2.length - 2] + array2[array2.length - 1]);
+    return fibonacciViaRecursion(n - 1);
+  }
+
+  console.log("!!!!", array2);
+
+  return array2[array2.length - 1];
+}
+
+console.log(fibonacciViaRecursion(10));
+
+const fibonacciCleanedFunction = (num) => {
+  if (num < 2) {
+    return num;
+  }
+  console.log(num);
+  return fibonacciCleanedFunction(num - 1) + fibonacciCleanedFunction(num - 2);
+};
+
+console.log("Cleaned code", fibonacciCleanedFunction(9));
